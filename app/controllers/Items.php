de095f4fb0a62c46cc9d99c15041aa4950605acb
valk20 <vkond97@gmail.com>
2023-05-20 12:38:01 +0300

    check response without db

--- app/controllers/Items.php
+++ app/controllers/Items.php
@@ -3,11 +3,18 @@
 class Items extends Controller
 {
     public function get($id) {
-
-
+        echo json_encode([
+           'data' => [
+               'id' => $id,
+               'name' => 'Test',
+               'phone' => '123456788',
+               'key' => 'key_value'
+           ]
+        ]);
     }
 
     public function store() {
+
 
     }
 
--- app/libraries/Controller.php
+++ app/libraries/Controller.php
@@ -1,5 +1,9 @@
 <?php
 
 class Controller {
+    public function __construct()
+    {
+        header("Content-Type:application/json");
+    }
 
 }
